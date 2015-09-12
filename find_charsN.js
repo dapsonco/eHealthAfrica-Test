87beafdad20aaf8ab11d7910e20d26612a215299
Dapo Adefila <dapsonco@users.noreply.github.com>
2015-09-12 23:53:21 +0100

    Update find_charsN.js

--- find_charsN.js
+++ find_charsN.js
@@ -2,7 +2,6 @@ function find_charsN(str1,str2){
   
   var st = str2.split("");
   var s = "";
-  //return st;
   for (var x = 0; x < str1.length; x++){
     if((st.indexOf(str1[x]) > -1)){
       s+=str1[x]

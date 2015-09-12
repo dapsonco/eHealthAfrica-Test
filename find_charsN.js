d9137ac4068c2bab75ce17b34d18b5f951a53000
Dapo Adefila <dapsonco@users.noreply.github.com>
2015-09-12 23:48:43 +0100

    Create find_charsN.js

---
+++ find_charsN.js
@@ -0,0 +1,12 @@
+function find_charsN(str1,str2){ 
+  
+  var st = str2.split("");
+  var s = "";
+  //return st;
+  for (var x = 0; x < str1.length; x++){
+    if((st.indexOf(str1[x]) > -1)){
+      s+=str1[x]
+    }
+  }
+  return s;
+}
